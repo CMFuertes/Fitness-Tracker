e3498ac8268bad6f45e9ec0d4cf901422263b12d
Cristhal Fuertes <cristhal.fuertes@gmail.com>
2020-10-14 17:51:26 -0400

    models

--- models/workouts.js
+++ models/workouts.js
@@ -3,13 +3,22 @@ const Schema = mongoose.Schema;
 
 const ExampleSchema = new Schema({
 
-    Day: {
+    day: {
         type: Date,
         default: Date.now
       },
 
+      exercises: [{
+        type: String,
+        name: String,
+        duration: Number,
+        weight: Number,
+        reps: Number,
+        sets: Number,
+        distance: Number, 
+    }]
 });
 
-const Example = mongoose.model("Example", ExampleSchema);
+const workouts = mongoose.model("Workouts", ExampleSchema);
 
-module.exports = Example;
+module.exports = workouts;
