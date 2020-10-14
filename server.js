8cc66318e4c3e26a82f4c91d80d33bcf26d1ed68
Cristhal Fuertes <cristhal.fuertes@gmail.com>
2020-10-14 14:00:06 -0400

    added routes, conected to mongoose

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false 
+});
+
+app.use(require("./routes/apiroutes.js"));
+app.use(require("./routes/htmlroutes.js"));
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
