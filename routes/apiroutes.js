a492ae7e932612d2460140264efe8780bee308a2
Cristhal Fuertes <cristhal.fuertes@gmail.com>
2020-10-14 14:08:38 -0400

    added route

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -10,3 +10,27 @@ router.post("/api/workouts", ({ body }, res) => {
         res.status(400).json(err);
       });
   });
+
+  
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+router.post("/api/workouts", ({ body }, res) => {
+    Workout.create({ body })
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+  
+  router.get("/api/workouts/range",function(req,res){  
+    Workout.find()
+    .then(dbWorkout =>{  
+        res.json(dbWorkout)
+    })
+    .catch(err => { 
+        res.json(err)
+    })
+});
