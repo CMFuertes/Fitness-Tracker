6feb74d54fe7843a1772465febf8e7f87ffc6593
Cristhal Fuertes <cristhal.fuertes@gmail.com>
2020-10-14 18:05:43 -0400

    updated routes

--- seeders/seeds.js
+++ seeders/seeds.js
@@ -125,7 +125,7 @@ let workoutSeed = [
   ];
   
   workouts.deleteMany({})
-    .then(() => db.Workout.collection.insertMany(workoutSeed))
+    .then(() => workouts.collection.insertMany(workoutSeed))
     .then(data => {
       console.log(data.result.n + " records inserted!");
       process.exit(0);
